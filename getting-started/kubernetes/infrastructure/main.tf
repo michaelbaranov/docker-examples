c2924621e4ecb0f0f9f9eb3131bbb25e3b682d28
michaelbaranov <1643817+michaelbaranov@users.noreply.github.com>
2021-04-07 14:58:42 +0300

    Made ACR name configurable

--- getting-started/kubernetes/infrastructure/main.tf
+++ getting-started/kubernetes/infrastructure/main.tf
@@ -124,7 +124,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "node_pools" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                     = "${var.deployment_prefix}acr${random_id.acr_name_suffix.dec}"
+  name                     = "${var.acrname}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   sku                      = "Standard"
