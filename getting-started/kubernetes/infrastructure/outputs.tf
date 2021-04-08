7bacad7f54332088e42ef9a78fc9212de0d4d575
michaelbaranov <1643817+michaelbaranov@users.noreply.github.com>
2021-04-08 18:19:43 +0300

    Obsolete variable removed

--- getting-started/kubernetes/infrastructure/outputs.tf
+++ getting-started/kubernetes/infrastructure/outputs.tf
@@ -1,11 +1,3 @@
 output "sql_fqdn" {
   value = azurerm_sql_server.sql.fully_qualified_domain_name
 }
-
-output "sql_admin_login" {
-  value = random_password.sql_admin_username.result
-}
-
-output "sql_admin_password" {
-  value = random_password.sql_admin_username.result
-}
