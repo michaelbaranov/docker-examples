0adfe968efecda5439e998c2c93f7d0a11c2da88
michaelbaranov <1643817+michaelbaranov@users.noreply.github.com>
2021-04-12 12:08:37 +0300

    sa is not allowed

--- getting-started/kubernetes/infrastructure/main.tf
+++ getting-started/kubernetes/infrastructure/main.tf
@@ -123,7 +123,7 @@ resource "azurerm_sql_server" "sql" {
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
-  administrator_login          = "sa"
+  administrator_login          = "sitecore"
   administrator_login_password = var.sql_admin_password
 }
 
