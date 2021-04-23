6dea19067c243d81aa0eb4bd090d3ace77e7966a
michaelbaranov <1643817+michaelbaranov@users.noreply.github.com>
2021-04-23 16:12:01 +0300

    Bump azurerm version

--- getting-started/kubernetes/infrastructure/main.tf
+++ getting-started/kubernetes/infrastructure/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = ">= 2.26"
+      version = ">= 2.56"
     }
     random = {
       source = "hashicorp/random"
